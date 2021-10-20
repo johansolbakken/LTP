c88849b8fcd7978cc61478c9458d65120a57c030
Johan Solbakken <Johsol@stud.ntnu.no>
2021-10-20 18:58:20 +0200

    Created base backend

---
+++ ltp-backend/src/main/java/no/johansolbakken/ltpbackend/LtpBackendApplication.java
@@ -0,0 +1,13 @@
+package no.johansolbakken.ltpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LtpBackendApplication.class, args);
+    }
+
+}
---
+++ ltp-backend/src/test/java/no/johansolbakken/ltpbackend/LtpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.johansolbakken.ltpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
